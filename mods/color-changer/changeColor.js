421b01510d17d6092637bdba3a64963027a64630
aoticbarker <92726972+aoticwise@users.noreply.github.com>
2021-12-01 08:35:44 -0500

    Update changeColor.js

--- mods/color-changer/changeColor.js
+++ mods/color-changer/changeColor.js
@@ -4,8 +4,8 @@
 var Mod = () => {
     colors.walls =      '#000000';
     colors.shadows =    '#000000';
-    colors.lightTiles = '#353536';
-    colors.darkTiles =  '#2b2b2b';
+    colors.lightTiles = '#000000';
+    colors.darkTiles =  '#000000';
     colors.bar =        '#000000';
 }
 
